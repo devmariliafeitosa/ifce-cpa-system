3d4d4312043faef84862d0fb058a7386e22aa284
l-Luiz-Henrique-l <luizhenriquef141205@gmail.com>
2026-09-07 19:33:00 -0300

    adição de configuração do Firebase e configuração de autenticação

--- backend/ifce-cpa-api/src/routes/usersRoutes.js
+++ backend/ifce-cpa-api/src/routes/usersRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
+const { authenticate, exigirRole } = require('../middlewares/authMiddleware');
 const usersController = require('../controllers/usersController');
-const { exigirRole } = require('../middlewares/authMiddleware');
 
-router.post('/', exigirRole('coordenador'), usersController.criar);
+router.post('/', authenticate, exigirRole('coordenador'), usersController.criar);
 
 router.get('/:id', usersController.buscarPorId);
 
